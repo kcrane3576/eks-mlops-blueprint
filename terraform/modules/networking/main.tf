49d72178fdc36c2348ddcf2566f5fcda2819c0b0
kcrane <kcrane121@gmail.com>
2025-07-26 13:25:19 -0400

    updated config to ensure clean notes about checkov skips

--- terraform/modules/networking/main.tf
+++ terraform/modules/networking/main.tf
@@ -2,6 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
+# Uses Terraform Registry version pinning (CKV_TF_1 skipped in .checkov.yml)
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "6.0.1"
@@ -39,6 +40,7 @@ module "vpc" {
   }, { Environment = var.environment })
 }
 
+# Restricts default SG traffic post-VPC creation (CKV2_AWS_12 skipped in .checkov.yml)
 resource "aws_default_security_group" "restrict_all" {
   vpc_id = module.vpc.vpc_id
 
@@ -102,6 +104,8 @@ resource "aws_network_acl" "custom" {
 }
 
 # Associate custom NACL with all private subnets (overrides default)
+# Each subnet is associated with custom NACL using count() (CKV2_AWS_1 skipped in .checkov.yml)
+# Flow logs and NAT EIPs are configured via input variables (CKV2_AWS_11, CKV2_AWS_19 skipped in .checkov.yml)
 resource "aws_network_acl_association" "private" {
   count = length(module.vpc.private_subnets)
 
